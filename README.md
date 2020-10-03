fd51f4871ddbae7c9d7d47dd387bac7a59db90eb
Luke Bentley-Fox <git@lukebentleyfox.net>
2020-10-03 01:04:12 +0100

    flakify

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# Get a recent nixops with plugins
+# NixOps Plugged
 
-Cloned from [nixopsenv from adisbladis](https://github.com/adisbladis/nixopsenv/).
+Flakified version of [nixops-with-plugins](https://github.com/typetetris/nixops-with-plugins/) by @typetetris.
+
+| Plugins | Included |
+|:---|:---:|
+| AWS           | :heavy_check_mark: |
+| DigitalOcean  | :heavy_check_mark: |
+| GCE           | :heavy_check_mark: |
+| Hetzner Robot | :x: |
+| Hetzner Cloud | :heavy_check_mark: |
+| Proxmox       | :heavy_check_mark: |
+| Virtd         | :heavy_check_mark: |
+| VBox          | :heavy_check_mark: |
+
+You can refer to this flake as input for another flake, i.e. inside the development environment for a flake packaging a NixOps network.
+```nix
+{
+  description = "Your awesome flake";
+  
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixops-plugged.url = "github:lukebfox/nixops-plugged";
+  inputs.utils.url = "github:numtide/flake-utils";
+  
+  outputs = {self, nixpkgs, nixops-plugged, utils, ...}:
+    let pkgs = import nixpkgs { inherit system; };
+    in {
+      nixopsConfigurations.default = { ... }; # your network definition
+    } // utils.lib.eachDefaultSystem (system: {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ nixops-plugged.defaultPackage.${system} ];
+      };
+    });
+}
+```
+
+To get a version of nixops with only the plugins you want, I would recommend forking this and following the instructions.
+It is packages as a flake anticipating the coming flake app
 
 Pinned nixpkgs version, so you can just use `default.nix` to build it or include
 it somewhere else.
@@ -9,15 +44,15 @@ Pinned nixpkgs should not imply, you have to use the nixpkgs version this was
 build with, anywhere else. It is just a python app being build and after
 that `nixops` should pick up, which `nixpkgs` to use the usual way it always does.
 
-Didn't get the [hetzner plugin](https://github.com/NixOS/nixops-hetzner) to work, sorry for that.
-
 The general steps to add a plugin:
 
 Install poetry in some way.
 
-Add your plugin to `pyproject.toml` and in `withplug.nix`.
+Add your plugin to `pyproject.toml` and in `defaultPackage`.
 
 Run `poetry lock`. If you made any errors editing `pyproject.toml` it should
 tell you.
 
-Run `nix-build`.
+Run `nix build`.
+
+---
---
+++ flake.nix
@@ -0,0 +1,133 @@
+{
+  description = "NixOps with several plugins installed.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs: inputs.utils.lib.eachDefaultSystem (system: let
+    
+    pkgs = import inputs.nixpkgs { inherit system; };
+
+    inherit (pkgs) lib;
+    
+    # Wrap the buildEnv derivation in an outer derivation that omits interpreters & other binaries
+    mkPluginDrv = { finalDrv, interpreter, plugins }:
+      let
+
+        # The complete buildEnv drv
+        buildEnvDrv = interpreter.buildEnv.override {
+          extraLibs = plugins;
+        };
+
+        # Create a separate environment aggregating the share directory
+        # This is done because we only want /share for the actual plugins
+        # and not for e.g. the python interpreter and other dependencies.
+        manEnv = pkgs.symlinkJoin {
+          name = "${finalDrv.pname}-with-plugins-share-${finalDrv.version}";
+          preferLocalBuild = true;
+          allowSubstitutes = false;
+          paths = plugins;
+          postBuild = ''
+          if test -e $out/share; then
+            mv $out out
+            mv out/share $out
+          else
+            rm -r $out
+            mkdir $out
+          fi
+          '';
+        };
+      in
+        pkgs.runCommandNoCC
+          "${finalDrv.pname}-with-plugins-${finalDrv.version}"
+          { inherit (finalDrv) passthru meta; }
+          ''
+          mkdir -p $out/bin
+
+          for bindir in ${lib.concatStringsSep " " (map (d: "${lib.getBin d}/bin") plugins)}; do
+            for bin in $bindir/*; do
+              ln -s ${buildEnvDrv}/bin/$(basename $bin) $out/bin/
+            done
+          done
+
+          ln -s ${manEnv} $out/share
+          '';
+
+    # Make a python derivation pluginable
+    #
+    # This adds a `withPlugins` function that works much like `withPackages`
+    # except it only links binaries from the explicit derivation /share
+    # from any plugins
+    toPluginAble = { drv, finalDrv, final }: drv.overridePythonAttrs(old: {
+      passthru = old.passthru // {
+        withPlugins = pluginFn: mkPluginDrv {
+          plugins = [ finalDrv ] ++ pluginFn final;
+          inherit finalDrv interpreter;
+        };
+      };
+    });
+
+    overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
+      # Make nixops pluggable
+      nixops = toPluginAble {
+        # Attach meta to nixops
+        drv = prev.nixops.overridePythonAttrs (old: {
+          format = "pyproject";
+          buildInputs = old.buildInputs ++ [ final.poetry ];
+          meta = old.meta // {
+            homepage = https://github.com/NixOS/nixops;
+            description = "NixOS cloud provisioning and deployment tool";
+            maintainers = with lib.maintainers; [ aminechikhaoui eelco rob domenkozar ];
+            platforms = lib.platforms.unix;
+            license = lib.licenses.lgpl3;
+          };
+        });
+        finalDrv = final.nixops;
+        inherit final;
+      };
+    });
+
+    interpreter = (pkgs.poetry2nix.mkPoetryPackages {
+      inherit overrides;
+      projectDir = ./.;
+    }).python;
+
+  in {
+
+    # A nixops with all plugins.
+    defaultPackage = interpreter.pkgs.nixops.withPlugins (ps: [
+      ps.nixops-aws
+      # ps.nixops-digitalocean
+      ps.nixops-gcp
+      ps.nixops-hetznercloud
+      # ps.nixops-proxmox
+      ps.nixops-virtd
+      ps.nixopsvbox
+    ]);
+
+    # A nixops for each plugin for those who just want a specific one.
+    packages = {
+      nixops-aws = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-aws]);
+      nixops-gcp = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-gcp]);
+      #nixops-digitalocean = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-digitalocean]);
+      nixops-hetznercloud = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-hetznercloud]);
+      #nixops-proxmox = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-proxmox]);
+      nixops-virtd = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixops-virtd]);
+      nixopsvbox = interpreter.pkgs.nixops.withPlugins (ps: [ps.nixopsvbox]);
+    };
+
+    # Can use this function to mix plugins as you see fit.
+    lib = interpreter.pkgs.nixops.withPlugins;
+
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.poetry
+        (pkgs.poetry2nix.mkPoetryEnv {
+          inherit overrides;
+          projectDir = ./.;
+        })
+      ];
+    };
+
+  });
+}
